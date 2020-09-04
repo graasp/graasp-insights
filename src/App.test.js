2a6d84e23c07e93e3903ecfc89ea09a0a508ee3b
pyphilia <pyphilia@gmail.com>
2020-09-04 10:59:45 +0200

    test: add sample App test

---
+++ src/App.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { App } from './App';
+
+describe('<App />', () => {
+  const props = {
+    i18n: {
+      defaultNS: '',
+      changeLanguage: jest.fn(),
+    },
+    t: jest.fn(),
+  };
+
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  const component = shallow(<App {...props} />);
+  it('renders correctly', () => {
+    expect(component).toMatchSnapshot();
+  });
+});
