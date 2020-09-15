4b5ace709e3967500addb12bab7f84932a6b3299
Hagop Taminian <hagoptaminian@gmail.com>
2020-09-15 15:36:13 +0300

    feat: clean up python.py code

--- public/python.py
+++ public/python.py
@@ -5,15 +5,6 @@ import json
 with open(sys.argv[1]) as json_file:
     allData = json.load(json_file)
     with open("anonymized_actions.json", "w+") as anonymizedFile:
-        anonymizedFile.write('{"actions": ['),
-        separator = '',
         for action in allData['data']['actions']:
-            action['user'] = hash(action['user']),
-            if (str(separator)[1:3] == repr('')):
-                json.dump(action, anonymizedFile),
-                separator = ','
-            else:
-                anonymizedFile.write(separator),
-                json.dump(action, anonymizedFile),
-        anonymizedFile.write(']}'),
-    anonymizedFile.close()
+            action['user'] = hash(action['user'])
+        json.dump(allData, anonymizedFile)
