fcb0dba614638144d45664481fd446450d75f2aa
Hagop Taminian <hagoptaminian@gmail.com>
2020-09-22 14:53:03 +0200

    chore: update python script to save in VAR_FOLDER

--- public/app/listeners/executePythonAlgorithm.js
+++ public/app/listeners/executePythonAlgorithm.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const { spawn } = require('child_process');
 const logger = require('../logger');
+const { VAR_FOLDER } = require('../config/config');
 
 const executePythonAlgorithm = () => {
   const process = spawn('python', [
     path.resolve(__dirname, '../../python.py'), // todo: this file should be a parameter
     'copy_of_data.json', // todo: this file path/name should be imported from global state (the file chosen for anonymizing)
+    `${VAR_FOLDER}/anonymized_data.json`, // todo: this file should be named based on the original file name and script run
   ]);
 
   process.stdout.on('data', (chunk) => {
--- public/python.py
+++ public/python.py
@@ -4,7 +4,7 @@ import json
 # expects sys.argv[1] to be path for json file, formatted as per graasp research
 with open(sys.argv[1]) as json_file:
     allData = json.load(json_file)
-    with open("anonymized_actions.json", "w+") as anonymizedFile:
+    with open(sys.argv[2], "w+") as anonymizedFile:
         for action in allData['data']['actions']:
             action['user'] = hash(action['user'])
         json.dump(allData, anonymizedFile)
