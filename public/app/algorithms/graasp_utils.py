30fa3deddb1419a135bb561ad990d5dc1cd77ea2
pyphilia <pyphilia@gmail.com>
2020-11-19 17:34:01 +0100

    fix: use open encoding depending on python version

--- public/app/algorithms/graasp_utils.py
+++ public/app/algorithms/graasp_utils.py
@@ -1,11 +1,18 @@
 import json, hashlib, argparse
 
+def open_file(file, permission = 'r'):
+    version = platform.python_version()[0]
+    if version == 3:
+        return open(file, permission, encoding="utf8")
+    else:
+        return open(file, permission)
+
 def load_dataset(dataset_path):
-    with open(dataset_path) as json_file:
+    with open_file(dataset_path) as json_file:
         return json.load(json_file)
 
 def save_dataset(dataset, dest_path):
-    with open(dest_path, 'w') as dest_file:
+    with open_file(dest_path, 'w') as dest_file:
         json.dump(dataset, dest_file, indent=2)
 
 def sha256_hash(value):
