f2c12c906d6ab4ffb1d3bd6c285e0046e2d40d0f
pyphilia <pyphilia@gmail.com>
2021-02-18 12:46:15 +0100

    refactor: fix logger info when opening path in explorer

--- public/app/listeners/openPathInExplorer.js
+++ public/app/listeners/openPathInExplorer.js
@@ -4,7 +4,7 @@ const path = require('path');
 const logger = require('../logger');
 
 const openPathInExplorer = () => (event, location) => {
-  logger.debug(`showing path ${path} in explorer`);
+  logger.debug(`showing path ${location} in explorer`);
   // path.resolve necessary for windows location path
   shell.showItemInFolder(path.resolve(location)); // Show the given file in a file manager. If possible, select the file.
 };
